cfd34f9564874ffcc7382e32eb562325decb159d
Anne Walch <anne.walch@yahoo.fr>
2026-01-14 14:13:41 +0100

    Ajout du test rétrogradation BDON-37

--- tests/annuaire-anne.spec.js
+++ tests/annuaire-anne.spec.js
@@ -11,7 +11,7 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
     // la page affiche "Bienvenue chez BugCorp"
     await page.getByRole("heading", { name: "Bienvenue chez BugCorp" }).click();
 
-    // Coffee Maker, Prompt Engineer, Scapegoat, Intern, Junior Dev, Senior Dev, Manager, Director, VP, CEO, Galactic Emperor  
+    // Coffee Maker, Prompt Engineer, Scapegoat, Intern, Junior Dev, Senior Dev, Manager, Director, VP, CEO, Galactic Emperor
 
     // Etape 2
     //Cliquer sur "L’Annuaire" sur le bandeau de gauche.
@@ -107,7 +107,7 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
     );
 
     // Etape 9
-// Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
+    // Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
     await page.getByTestId("promote-btn-1015").click();
     // Le nom du rôle situé sous l’identité de l’employé change pour devenir: Manager
     await expect(page.locator("#cell-role-1015")).toContainText("Manager");
@@ -118,9 +118,8 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
       "-90 000 €"
     );
 
-
     // Etape 10
-// Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
+    // Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
     await page.getByTestId("promote-btn-1015").click();
     // Le nom du rôle situé sous l’identité de l’employé change pour devenir: Director
     await expect(page.locator("#cell-role-1015")).toContainText("Director");
@@ -131,9 +130,8 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
       "-105 000 €"
     );
 
-
-// Etape 11
-// Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
+    // Etape 11
+    // Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
     await page.getByTestId("promote-btn-1015").click();
     // Le nom du rôle situé sous l’identité de l’employé change pour devenir: VP
     await expect(page.locator("#cell-role-1015")).toContainText("VP");
@@ -144,8 +142,8 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
       "-120 000 €"
     );
 
-// Etape 12
-// Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
+    // Etape 12
+    // Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
     await page.getByTestId("promote-btn-1015").click();
     // Le nom du rôle situé sous l’identité de l’employé change pour devenir: CEO
     await expect(page.locator("#cell-role-1015")).toContainText("CEO");
@@ -156,12 +154,13 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
       "-135 000 €"
     );
 
-
-// Etape 13
-// Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
+    // Etape 13
+    // Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
     await page.getByTestId("promote-btn-1015").click();
     // Le nom du rôle situé sous l’identité de l’employé change pour devenir: Galactic Emperor
-    await expect(page.locator("#cell-role-1015")).toContainText("Galactic Emperor");
+    await expect(page.locator("#cell-role-1015")).toContainText(
+      "Galactic Emperor"
+    );
     // le nombre de bugs a changé
     await expect(page.locator("#cell-bugs-1015")).not.toHaveText("-134");
     // le  montant inscrit sous objectif d'économie selon le montant prévu est modifié
@@ -169,20 +168,76 @@ test.describe("Changer le rôle d'un employé: promouvoir", () => {
       "-150 000 €"
     );
 
-
-
-// Etape 14
-// Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
+    // Etape 14
+    // Cliquer sur l’icône apparaissant sous la forme d’un bouton contenant une flèche verte en escalier vers le haut.
     await page.getByTestId("promote-btn-1015").click();
     // Le nom du rôle situé sous l’identité de l’employé change pour devenir: Galactic Emperor
-    await expect(page.locator("#cell-role-1015")).toContainText("Galactic Emperor");
+    await expect(page.locator("#cell-role-1015")).toContainText(
+      "Galactic Emperor"
+    );
     // le nombre de bugs a changé
     await expect(page.locator("#cell-bugs-1015")).toHaveText("-144");
     // le  montant inscrit sous objectif d'économie selon le montant prévu est modifié
     await expect(page.locator("#current-savings-value")).toContainText(
       "-150 000 €"
     );
+  });
 
+  test("test BDON-37", async ({ page }) => {
+    // Etape 1
+    // Visiter le site
+    await page.goto("https://bugcorp.vercel.app/");
+    //Dans le bandeau de gauche, "Accueil" apparaît sur un fond bleu
+    await expect(page.getByTestId("nav-home")).toBeVisible();
+    // la page affiche "Bienvenue chez BugCorp"
+    await page.getByRole("heading", { name: "Bienvenue chez BugCorp" }).click();
 
+    // Etape 2
+    //Cliquer sur "L’Annuaire" sur le bandeau de gauche.
+    await page.getByTestId("nav-directory").click();
+    // La page ayant pour titre "L’annuaire Enterprise" s’affiche.
+    await expect(
+      page.getByRole("heading", { name: "L'Annuaire Enterprise" })
+    ).toBeVisible();
+    //  Elle contient :un encart nommé "Objectif d'économies (restructuration)
+    await expect(
+      page.getByRole("heading", { name: "Objectif d'Économies (" })
+    ).toBeVisible();
+    //avec en dessous, un montant en euro
+    await expect(page.locator("#target-savings-value")).toContainText("€");
+
+    // Etape 3
+    // Placer la souris au croisement de la ligne correspondant à l’ID de l'employé sélectionné et de la colonne "actions".
+    // la liste doit afficher l'employé à tester et son rôle
+    // ID à tester: #1010 Ellen Ripley - rôle de base: Galactic Emperor
+    await expect(page.locator("#cell-name-1010")).toContainText("Ellen Ripley");
+    await expect(page.locator("#cell-role-1010")).toContainText(
+      "Galactic Emperor"
+    );
+    // au survol avec la souris, le bouton "Rétrograder" doit apparaître
+    await page.getByTestId("demote-btn-1010").hover();
+    await expect(page.getByTestId("demote-btn-1010")).toBeVisible();
+    // récupérer le nombre de BUGS affiché
+    const bugsLocator = page.locator("#cell-bugs-1010");
+
+    const initialText = await bugsLocator.textContent();
+    let bugCount = Number(initialText); // ex: +128 => 128
+    console.log("nb de bugs: ", bugCount);
+    // vérifier que le nombre de bug attendu correspond à ce que l'on récupère
+    expect(Number(initialText)).toBe(bugCount);
+
+    // L’encart  objectif d'économie indique: OBJECTIF D’ECONOMIES = 0€ / "objectif d'économie défini"
+    // Localiser l'élément
+    const savingsLocator = page.locator("#current-savings-value");
+
+    // Récupérer le texte
+    const text = await savingsLocator.textContent(); // "0 €" (note: le &nbsp; devient un vrai espace)
+
+    // Extraire le nombre
+    let savings = Number(text?.replace(/[^\d.-]/g, ""));
+
+    console.log("OBJECTIF D’ECONOMIES", savings); // 0
+
+    await page.getByText("0 €", { exact: true }).click();
   });
 });
